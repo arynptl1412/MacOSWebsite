2f9e9adc16f602577ac5ff4df224da00e65ba9e5
Aryan Patel <arynptl1412@gmail.com>
2026-02-19 22:12:28 +0530

    Resizable Component Added

--- MacOS/src/App.jsx
+++ MacOS/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Dock from './components/Dock'
 import Navbar from './components/Navbar'
+import MacWindow from './components/windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Dock/>
-      
+      <MacWindow>
+        hello
+      </MacWindow>
     </main>
   )
 }
